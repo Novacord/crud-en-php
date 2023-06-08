4cc15d3afa88214c801881b206f889490fa60136
JoseDanielNovaMuñozCAMPUS <josedanielnova2027@gmail.com>
2023-06-08 09:26:15 -0500

    feat: :art: maquetacion crud
    termine la maquetacion del crud y lo modifique para ponerlo a funcion con php

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
 </head>
 <body>
 
-    <section>
+    <form>
         <div class="container text-center">
             <div class="row row-invert">
                 <div class="col colCenter">
-                    <input type="text" placeholder="Nombre" name="identificacion" required>
+                    <input type="text" placeholder="Nombre" name="perfil[nombre]" autocomplete="off">
                 </div>
                 <div class="col">
                     <h1>Campuslads</h1>
@@ -28,34 +28,69 @@
             </div>
             <div class="row">
                 <div class="col">
-                    <input type="text" placeholder="Apellido" name="identificacion" required>
+                    <input type="text" placeholder="Apellido" name="perfil[apellido]" autocomplete="off">
                 </div>
                 <div class="col">
-                    <input type="text" placeholder="Edad" name="identificacion" required>
+                    <input type="text" placeholder="Edad" name="perfil[edad]" autocomplete="off">
                 </div>
             </div>
             <div class="row">
                 <div class="col">
-                    <input type="text" placeholder="Direccion" name="identificacion" required>
+                    <input type="text" placeholder="Direccion" name="perfil[direccion]" autocomplete="off">
                 </div>
                 <div class="col">
-                    <input type="text" placeholder="Email" name="identificacion" required>
+                    <input type="text" placeholder="Email" name="perfil[email]" autocomplete="off">
                 </div>
             </div>
             <div class="row">
                 <div class="col d-flex flex-column gap-2">
-                    <input type="text" placeholder="Horario de entrada" name="identificacion" required>
-                    <input type="text" placeholder="Horario de entrada" name="identificacion" required>
-                    <input type="text" placeholder="Horario de entrada" name="identificacion" required>
+                    <input type="text" placeholder="Horario de entrada" name="perfil[horarioEntrada]" autocomplete="off">
+                    <input type="text" placeholder="Team" name="perfil[team]" autocomplete="off">
+                    <input type="text" placeholder="Trainer" name="perfil[trainer]" autocomplete="off">
                 </div>
-                <div class="col gap-2">
-                    <div>
+                <div class="col gap-3">
+                    <div class="gap">
                         <button><i class="bi bi-check-lg"></i></button>
                         <button><i class="bi bi-x"></i></button>
                     </div>
+                    <div class="gap">
+                        <button><i class="bi bi-pencil"></i></button>
+                        <button><i class="bi bi-search"></i></button>
+                    </div>
+                    <div class="gap">
+                        <input type="text" placeholder="Cedula" name="perfil[cedula]" >
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col" id="sector-con-scroll">
                     <div>
-                        <button><i class="bi bi-check-lg"></i></button>
-                        <button><i class="bi bi-x"></i></button>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Dirección</th>
+                                <th>Edad</th>
+                                <th>Email</th>
+                                <th>Horario de entrada</th>
+                                <th>Team</th>
+                                <th>Trainer</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>John</td>
+                                <td>Doe</td>
+                                <td>123 Calle Principal</td>
+                                <td>25</td>
+                                <td>johndoe@example.com</td>
+                                <td>9:00 AM</td>
+                                <td>Team A</td>
+                                <td>Trainer 1</td>
+                            </tr>
+                        </tbody>
+                    </table>
                     </div>
                 </div>
             </div>
