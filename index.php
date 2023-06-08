6b01b015b46b029f338944638e37a07c1f34e513
JoseDanielNovaMuñozCAMPUS <josedanielnova2027@gmail.com>
2023-06-08 12:13:22 -0500

    feat: :construction: crud en php
    teremine que guarde que elimine y que muestre me falta terminar y que esta en progreso que edite y me falta que busque

--- index.php
+++ index.php
@@ -1,6 +1,124 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['agregar'])) {
+        $credenciales["http"]["method"] = "POST";
+        $credenciales["http"]["header"] = "Content-type: application/json";
+        $data = [
+            "nombre" => $_POST['perfil']['nombre'],
+            "apellido" => $_POST['perfil']['apellido'],
+            "direcccion" => $_POST['perfil']['direccion'],
+            "edad" => $_POST['perfil']['edad'],
+            "email" => $_POST['perfil']['email'],
+            "horarioEntrada" => $_POST['perfil']['horarioEntrada'],
+            "team" => $_POST['perfil']['team'],
+            "trainer" => $_POST['perfil']['trainer'],
+            "cc" => $_POST['perfil']['cedula'],
+        ];
+        $data = json_encode($data);
+        $credenciales["http"]["content"] = $data;
+        $config = stream_context_create($credenciales);
+
+        $_DATA = file_get_contents("https://6481e26229fa1c5c50323e5f.mockapi.io/formulario", false, $config);
+
+    }
+    if (isset($_POST['eliminar'])) {
+  
+        $cedulaEliminar = $_POST['perfil']['cedula'];
+
+        $url = 'https://6481e26229fa1c5c50323e5f.mockapi.io/formulario';
+        $data = file_get_contents($url);
+        $response = json_decode($data, true);
+
+        $idEliminar = null;
+        foreach ($response as $item) {
+            if ($item['cc'] === $cedulaEliminar) {
+                $idEliminar = $item['id'];
+                break;
+            }
+        }
+
+        if ($idEliminar !== null) {
+            $urlEliminar = $url . '/' . $idEliminar;
+            $credencialesD["http"]["method"] = "DELETE";
+            $credencialesD["http"]["header"] = "Content-type: application/json";
+            $config = stream_context_create($credencialesD);
+            $resultado = file_get_contents($urlEliminar, false, $config);
+
+        }
+        
+    }
+    if (isset($_POST['dar'])) {
+        if (isset($_POST['editar'])) {
+            $url = 'https://6481e26229fa1c5c50323e5f.mockapi.io/formulario/' . $_POST['dar'];
+            $data = [
+                "nombre" => $_POST['perfil']['nombre'],
+                "apellido" => $_POST['perfil']['apellido'],
+                "direccion" => $_POST['perfil']['direccion'],
+                "edad" => $_POST['perfil']['edad'],
+                "email" => $_POST['perfil']['email'],
+                "horarioEntrada" => $_POST['perfil']['horarioEntrada'],
+                "team" => $_POST['perfil']['team'],
+                "trainer" => $_POST['perfil']['trainer'],
+                "cc" => $_POST['perfil']['cc'],
+            ];
+            $data = json_encode($data);
+    
+            $credenciales["http"]["method"] = "PUT";
+            $credenciales["http"]["header"] = "Content-type: application/json";
+            $credenciales["http"]["content"] = $data;
+            $config = stream_context_create($credenciales);
+    
+            $resultado = file_get_contents($url, false, $config);
+        }
+    }
+
+    if (isset($_POST['dar'])) {
+        $urlG = 'https://6481e26229fa1c5c50323e5f.mockapi.io/formulario';
+        $urlGet = $urlG . '/' . $_POST['dar'];
+        $credencialesG["http"]["method"] = "GET";
+        $credencialesG["http"]["header"] = "Content-type: application/json";
+        $config = stream_context_create($credencialesG);
+        $resultado = file_get_contents($urlGet, false, $config);
+        $data = json_decode($resultado, true);
+
+        $nombre = $data['nombre'];
+        $apellido = $data['apellido'];
+        $direccion = $data['direcccion'];
+        $edad = $data['edad'];
+        $email = $data['email'];
+        $horarioEntrada = $data['horarioEntrada'];
+        $team = $data['team'];
+        $trainer = $data['trainer'];
+        $cc = $data['cc'];
+}
+}
+    
+    $url = 'https://6481e26229fa1c5c50323e5f.mockapi.io/formulario';
+    $data = file_get_contents($url);
+    $response = json_decode($data, true);
+
+    $tabla = '';
+
+    if (is_array($response)) {
+        foreach ($response as $item) {
+            $tabla .= '
+                <tr>
+                    <td>'.$item['nombre'].'</td>
+                    <td>'.$item['apellido'].'</td>
+                    <td>'.$item['direcccion'].'</td>
+                    <td>'.$item['edad'].'</td>
+                    <td>'.$item['email'].'</td>
+                    <td>'.$item['horarioEntrada'].'</td>
+                    <td>'.$item['team'].'</td>
+                    <td>'.$item['trainer'].'</td>
+                    <td><button class="extraer" type="submit" name="dar" value="'.$item['id'].'"><i class="bi bi-caret-up-fill"></i></button></td>
+                </tr>
+            ';
+        }
+    }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -16,11 +134,11 @@
 </head>
 <body>
 
-    <form>
+    <form method="POST">
         <div class="container text-center">
             <div class="row row-invert">
                 <div class="col colCenter">
-                    <input type="text" placeholder="Nombre" name="perfil[nombre]" autocomplete="off">
+                    <input type="text" placeholder="Nombre" name="perfil[nombre]" autocomplete="off" value="<?php echo isset($nombre) ? $nombre : null; ?>">
                 </div>
                 <div class="col">
                     <h1>Campuslads</h1>
@@ -28,37 +146,37 @@
             </div>
             <div class="row">
                 <div class="col">
-                    <input type="text" placeholder="Apellido" name="perfil[apellido]" autocomplete="off">
+                    <input type="text" placeholder="Apellido" name="perfil[apellido]" autocomplete="off" value="<?php echo isset($apellido) ? $apellido : null; ?>">
                 </div>
                 <div class="col">
-                    <input type="text" placeholder="Edad" name="perfil[edad]" autocomplete="off">
+                    <input type="text" placeholder="Edad" name="perfil[edad]" autocomplete="off" value="<?php echo isset($edad) ? $edad :null;?>">
                 </div>
             </div>
             <div class="row">
                 <div class="col">
-                    <input type="text" placeholder="Direccion" name="perfil[direccion]" autocomplete="off">
+                    <input type="text" placeholder="Direccion" name="perfil[direccion]" autocomplete="off" value="<?php echo isset($direccion) ? $direccion :null;?>">
                 </div>
                 <div class="col">
-                    <input type="text" placeholder="Email" name="perfil[email]" autocomplete="off">
+                    <input type="text" placeholder="Email" name="perfil[email]" autocomplete="off" value="<?php echo isset($email) ? $email :null;?>">
                 </div>
             </div>
             <div class="row">
                 <div class="col d-flex flex-column gap-2">
-                    <input type="text" placeholder="Horario de entrada" name="perfil[horarioEntrada]" autocomplete="off">
-                    <input type="text" placeholder="Team" name="perfil[team]" autocomplete="off">
-                    <input type="text" placeholder="Trainer" name="perfil[trainer]" autocomplete="off">
+                    <input type="text" placeholder="Horario de entrada" name="perfil[horarioEntrada]" autocomplete="off" value="<?php echo isset($horarioEntrada) ? $horarioEntrada :null;?>">
+                    <input type="text" placeholder="Team" name="perfil[team]" autocomplete="off" value="<?php echo isset($team) ? $team :null;?>">
+                    <input type="text" placeholder="Trainer" name="perfil[trainer]" autocomplete="off" value="<?php echo isset($trainer) ? $trainer :null;?>">
                 </div>
                 <div class="col gap-3">
                     <div class="gap">
-                        <button><i class="bi bi-check-lg"></i></button>
-                        <button><i class="bi bi-x"></i></button>
+                        <button type="submit" name="agregar" value="Botón 1"><i class="bi bi-check-lg"></i></button>
+                        <button type="submit" name="eliminar" value="Botón 2"><i class="bi bi-x"></i></button>
                     </div>
                     <div class="gap">
-                        <button><i class="bi bi-pencil"></i></button>
+                        <button type="submit" name="editar" value="Botón 3"><i class="bi bi-pencil"></i></button>
                         <button><i class="bi bi-search"></i></button>
                     </div>
                     <div class="gap">
-                        <input type="text" placeholder="Cedula" name="perfil[cedula]" >
+                        <input type="text" placeholder="Cedula" name="perfil[cedula]" autocomplete="off" value="<?php echo isset($cc) ? $cc : null; ?>">
                     </div>
                 </div>
             </div>
@@ -76,25 +194,20 @@
                                 <th>Horario de entrada</th>
                                 <th>Team</th>
                                 <th>Trainer</th>
+                                <th>extraer</th>
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td>John</td>
-                                <td>Doe</td>
-                                <td>123 Calle Principal</td>
-                                <td>25</td>
-                                <td>johndoe@example.com</td>
-                                <td>9:00 AM</td>
-                                <td>Team A</td>
-                                <td>Trainer 1</td>
-                            </tr>
+                            <?php if(isset($tabla)){ 
+                                echo $tabla; 
+                            } 
+                            ?>
                         </tbody>
                     </table>
                     </div>
                 </div>
             </div>
-    </section>
+    </form>
 
 </body>
 </html>
